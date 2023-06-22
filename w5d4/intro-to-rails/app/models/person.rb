33ae61f1f4d5c97b3c7550363a413fa054740c1c
stefanlazarevic <stefanlaza97@gmail.com>
2023-06-21 21:36:13 -0700

    add-w5d4

---
+++ w5d4/intro-to-rails/app/models/person.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: people
+#
+# id            :bigint         not null, primary key
+# name          :string         not null
+# house_id      :bigint         not null
+# created_at    :datetime       not null
+# updated_at    :datetime       not null
+
+class Person < ApplicationRecord
+    validates :name, :presence: true
+
+    belongs_to :house,
+        primary_key: :id,
+        foreign_key: :house_id,
+        class_name: :house
+end
