7164f1082c1c2dd5ce2c34c3a259654207cf816b
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-12 22:46:08 -0700

    add intro-to-canvas

---
+++ w8d3/intro-to-canvas/canvas-practice.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const canvasEle = document.getElementById("myCanvas")
+    canvasEle.width = 500;
+    canvasEle.height = 500;
+    
+});
